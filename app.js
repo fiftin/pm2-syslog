3022c505dbf31fea685999a22502163844eb0355
Denis Gukov <denguk@gmail.com>
2017-03-27 21:11:36 +0500

    feat: JSON lormatted logs

--- app.js
+++ app.js
@@ -3,22 +3,37 @@ var pm2       = require('pm2');
 var SysLogger = require('ain2');
 var logger    = new SysLogger({tag: 'pm2',  facility: 'syslog'});
 
+function logDataToString(data) {
+  let ret;
+  if (data.data) {
+    ret = {
+      app: data.process.name,
+      target_app: data.process.pm_id,
+      restart_count: data.process.restart_time,
+      status: data.event
+    };
+  } else {
+    ret = {
+      app: data.process.name,
+      id: data.process.pm_id,
+      data: data.data
+    };
+  }
+  return JSON.stringify(ret, null, '  ');
+}
+
 pm2.launchBus(function(err, bus) {
   bus.on('*', function(event, data){
     if (event == 'process:event') {
-      logger.warn('app=pm2 target_app=%s target_id=%s restart_count=%s status=%s',
-                  data.process.name,
-                  data.process.pm_id,
-                  data.process.restart_time,
-                  data.event);
+      logger.warn(logDataToString(data));
     }
   });
 
   bus.on('log:err', function(data) {
-    logger.error('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
+    logger.warn(logDataToString(data));
   });
 
   bus.on('log:out', function(data) {
-    logger.log('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
+    logger.warn(logDataToString(data));
   });
 });
